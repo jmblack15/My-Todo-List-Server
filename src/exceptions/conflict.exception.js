97bb25e73fe9be95d1d2440ee4b0280b38a6f51d
Jose Manuel Osorio <jm157653@gmail.com>
2025-11-15 12:40:47 -0500

    feature: create user endpoint

---
+++ src/exceptions/conflict.exception.js
@@ -0,0 +1,8 @@
+import { StatusCodes } from "http-status-codes";
+import BaseException from "./base.exception.js";
+
+export default class ConflictException extends BaseException {
+	constructor(message = "Conflict", errorCode = "CONFLICT_ERROR") {
+		super(message, StatusCodes.CONFLICT, errorCode);
+	}
+}
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,12 +1,42 @@
-const UserService = () => { 
+import { prisma } from "../models/prismaClient.js";
+import { MESSAGES } from "../utils/messages.utils.js";
+import ConflictException from "../exceptions/conflict.exception.js";
+import { hash } from "bcrypt";
 
-  const createUser = async (userData) => {
-    
-   }
+const UserService = () => {
+	const createUser = async (userData) => {
+		const { email } = userData;
+		const userExists = await findUserByEmail(email);
 
-  return {
-    createUser
-  };
-}
+		if (userExists) {
+			throw new ConflictException(MESSAGES.USER.ALREADY_EXISTS);
+		}
+
+		const { password } = userData;
+		const hashedPassword = await hash(password, 10);
+
+		const newUser = await prisma.user.create({
+			data: { ...userData, password: hashedPassword },
+		});
+
+		return {
+			id: newUser.id,
+			email: newUser.email,
+			name: newUser.name,
+			createdAt: newUser.createdAt,
+			updatedAt: newUser.updatedAt,
+		};
+	};
+
+	const findUserByEmail = async (email) => {
+		return await prisma.user.findUnique({
+			where: { email },
+		});
+	};
+
+	return {
+		createUser,
+	};
+};
 
 export { UserService };
