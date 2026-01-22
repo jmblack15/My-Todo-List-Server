59696fc245a0a1ebbac64663ccfc46edeeebebcf
Jose Manuel Osorio <jm157653@gmail.com>
2026-01-22 18:01:58 -0500

    fix: fix error prismaClien for v7

--- src/models/prismaClient.js
+++ src/models/prismaClient.js
@@ -1,2 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import "dotenv/config";
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
